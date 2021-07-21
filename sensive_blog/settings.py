774544bae893d73f5bce99edc90a0a8e989e6237
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-07-21 14:55:30 +0300

    set up the "static"

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -94,11 +94,11 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "/Users/gosavinokurov/python3.6/bin"), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
-STATIC_ROOT = os.getenv("STATIC_ROOT", "assets")
+STATIC_ROOT = os.getenv("STATIC_ROOT",'/static/')
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
